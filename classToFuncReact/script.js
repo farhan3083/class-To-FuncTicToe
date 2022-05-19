aaa7f1788216302c8a372de2a4a3b7e82131af89
farhan3083 <80308474+farhan3083@users.noreply.github.com>
2022-05-19 16:18:02 +0530

    Add files via upload

---
+++ classToFuncReact/script.js
@@ -0,0 +1,128 @@
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+function Board() {
+  const[squares,setSquares] =React.useState(Array(9).fill(null));
+  const[xIsNext,setxIsNext] =React.useState(true);
+  
+   handleClick(i) {
+    const squares = squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? 'X' : 'O';
+   }
+  
+  
+ function renderSquare(i) {
+  return <Square
+    value={squares[i]}
+    onClick={() => {
+      const nextSquares = squares.slice();
+      nextSquares[i] = isXNext ? "X" : "O";
+      setSquares(nextSquares);
+             
+      setxIsNext(!xIsNext);
+    }}
+  />;
+}
+const winner = calculateWinner(squares);
+
+function getStatus(){
+  let status;
+  if (winner) {
+      status = 'Winner: ' + winner;
+    } 
+  else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+   return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
+  
+  
+ }
+}
+
+function Game() {
+  
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{getStatus()}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
